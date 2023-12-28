0cb8a0e200bb7ec7c855fb8b561fb3cd05ee1d8f
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-28 22:14:55 +0100

    modified app.js

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const squares = document.querySelectorAll(".grid div");
 const result = document.querySelector("#result");
 const displayCurrentPlayer = document.querySelector("#current-player");
 let currentPlayer = 1;
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].onClick = () => {
+    alert("You have clicked square " + i);
+  };
+}
